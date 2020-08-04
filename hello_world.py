6c592e1d716a59915af730b108c4ce76ccdc0b77
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-03 21:47:54 -0500

    v1.1 Test

--- hello_world.py
+++ hello_world.py
@@ -5,9 +5,10 @@ from selenium import webdriver
 class HelloWorld(unittest.TestCase):
 
 	#Prepara el entorno de la prueba
-	def setUp(self):
-		self.driver = webdriver.Edge(executable_path = r'C://Users/N0S4A2/Documents/Platzi/Python/Selenium/Driver/msedgedriver')
-		driver = self.driver
+	@classmethod
+	def setUpClass(cls):
+		cls.driver = webdriver.Edge(executable_path = r'./Driver/msedgedriver')
+		driver = cls.driver
 		driver.implicitly_wait(10)
 
 
@@ -15,10 +16,16 @@ class HelloWorld(unittest.TestCase):
 	def test_hello_worl(self):
 		driver=self.driver
 		driver.get('https://www.platzi.com')
+		driver.get('https://www.wikipedia.org')
 
+
+	@classmethod
 	#Realiza las acciones finales
-	def tearDown(self):
-		self.driver.quit()
+	def tearDownClass(cls):
+		cls.driver.quit()
 
 if __name__ == "__main__":
+
+	#Verbosity funciona para mostrar ciertos logs:
+	# 0: Cuantos Tests corrió, 1: Lo mismo que 0 pero agrega el error, 2:cada test y cada resultado
 	unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name='hello-world-report'))
