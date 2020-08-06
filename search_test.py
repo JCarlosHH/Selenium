f02992cf0f2cefde138c16f99967fb730d2c2672
Carlos Hernandez <carloshhyh@gmail.com>
2020-08-05 22:34:12 -0500

    v1.4 Test

--- search_test.py
+++ search_test.py
@@ -10,7 +10,7 @@ class HomePageTest(unittest.TestCase):
 		self.driver = webdriver.Edge(executable_path = r'./Driver/msedgedriver.exe')
 		driver = self.driver
 		driver.get('http://demo.onestepcheckout.com/')
-		driver.maximize_window()
+		driver.maximize_window() #Maximiza la ventana
 		driver.implicitly_wait(15)
 
 
@@ -20,7 +20,7 @@ class HomePageTest(unittest.TestCase):
 
 
 	#def test_serch_text_field_by_name(self):
-	#	search_field = self.driver.find_element_by_name("q")
+	# 	search_field = self.driver.find_element_by_name("q")
 
 
 	#def test_serch_text_field_class_name(self):
@@ -31,10 +31,18 @@ class HomePageTest(unittest.TestCase):
 	#	button = self.driver.find_element_by_class_name("button")
 
 
-	def test_count_of_promo_banner_images(self):
-		banner_list = self.driver.find_element_by_class_name("promos")
-		banners = banner_list.find_element_by_tag_name('img')
-		self.assertEqual(3, len(banners))
+	#def test_count_of_promo_banner_images(self):
+	#	banner_list = self.driver.find_element_by_class_name("promos")
+	#	banners = banner_list.find_elements_by_tag_name('img')
+	#	self.assertEqual(3, len(banners)) #->verifica si una condición se cumple o no 
+
+
+	#por xpath
+	#def test_vip_promo(self):
+	#	vip_promo = self.driver.find_element_by_xpath('//*[@id="top"]/body/div[1]/div[2]/div[2]/div/div/div[2]/div/ul/li[2]/a/img')
+
+	def test_shopping_car(self):
+		shopping_cart_icon = self.driver.find_element_by_css_selector("div.header-minicart span.icon")
 
 
 	#Realiza las acciones finales
